7e22766cb0931ae11639607bd742d640ddf8350d
IroniX2 <57399961+IroniX2@users.noreply.github.com>
2021-10-20 22:02:57 +0200

    refactor entire binary to accept paths instead of searching the entire system

--- src/main.rs
+++ src/main.rs
@@ -1,118 +1,129 @@
 use std::{
     env,
     fs::File,
-    io::{BufRead, BufReader, Read, Write},
+    io::{BufRead, BufReader, Write},
     path::PathBuf,
 };
 use walkdir::WalkDir;
 mod custom_error;
 use custom_error::CustomError;
 
+// Ideal usage: <binary> -s /source/path -t /target/path -n index number
+const APP_NAME: &str = "versionator";
+
 fn main() -> Result<(), CustomError> {
     let args: Vec<String> = env::args().collect();
-    let working_dir = working_dir_params(args)?;
-    let (source_file_name, source_search_word) = ("package.json", "\"version\"");
-    let (target_file_name, target_search_word) = ("Pom.xml", "<version>");
-
-    let (source_path, target_path) =
-        get_file_paths(target_file_name, source_file_name, &working_dir)?;
-    let source_file = File::open(source_path).expect("unable to open source file 😓");
-
-    println!("Searching {} for the version.", &source_file_name);
-    let version = version_from_package_json(source_search_word, source_file)?;
-    println!("Version found: {}", version);
-
-    let target_file = File::open(&target_path).expect("unable to open target file 😓");
-    println!(
-        "Searching {} for the version tag to replace",
-        &source_file_name
-    );
-
-    let fixed_version = replace_pom_version(target_search_word, &version, target_file)?;
-    let mut target_file_write = File::create(target_path).expect("unable to open target file 😓");
-    target_file_write
-        .write_all(fixed_version.as_bytes())
-        .expect("Failed to save to file 😱");
-
-    println!("Main end");
-    Ok(())
-}
-
-fn search_for_file(file_name: &str, path: &PathBuf) -> Result<PathBuf, CustomError> {
-    if path.exists() {
-        for entry in WalkDir::new(path) {
-            let entry = entry.unwrap();
-            if entry.file_name().to_str().unwrap().contains(file_name) {
-                return Ok(entry.into_path());
-            }
-        }
-    }
-
-    Err(CustomError::FileNotFound)
-}
-
-fn get_file_paths(
-    target_name: &str,
-    source_name: &str,
-    path: &PathBuf,
-) -> Result<(PathBuf, PathBuf), CustomError> {
-    let source_path_result = search_for_file(source_name, &path);
-    let target_path_result = search_for_file(target_name, &path);
-    let (source_path, target_path) = match (source_path_result, target_path_result) {
-        (Ok(s_path), Ok(t_path)) => {
-            println!("File '{}' found at {:?}", &source_name, &s_path);
-            println!("File '{}' found at {:?}", &target_name, &t_path);
-            (s_path, t_path)
-        }
-        (Err(s_err), Err(t_err)) => {
+    let (source, target, version_instance_count) = match handle_params(args) {
+        Ok(tuple) => tuple,
+        Err(error) => return Err(error),
+    };
+    let search_word = "version";
+    let match_word = "<version>";
+    let source_file = match File::open(&source) {
+        Ok(file) => file,
+        Err(_) => {
             println!(
-                "Not able to find either file: {:?}, {:?}",
-                &target_name, &source_name
+                "Path does not point to a package.json | Path: {:?}",
+                &source
             );
-            println!("Errors:  {:?}, {:?}", &s_err, t_err);
-            println!("Path searched: {:?}", &path);
             return Err(CustomError::FileNotFound);
         }
-        (Ok(_), Err(t_err)) => {
-            println!("Target path not found for: {:?}", &target_name);
-            println!("Error: {:?}", &t_err);
-            println!("Path searched: {:?}", &path);
-            return Err(CustomError::TargetNotFound);
-        }
-        (Err(s_err), Ok(_)) => {
-            println!("Source path not found: {:?}", &source_name);
-            println!("Error: {:?}", &s_err);
-            println!("Path searched: {:?}", &path);
-            return Err(CustomError::SourceNotFound);
+    };
+    let target_file = match File::open(&target) {
+        Ok(file) => file,
+        Err(_) => {
+            println!("Path does not point to a pom.xml | Path: {:?}", &target);
+            return Err(CustomError::FileNotFound);
         }
     };
-    return Ok((source_path, target_path));
+    let version = match version_from_package_json(&search_word, &source_file) {
+        Ok(version) => version,
+        Err(error) => {
+            println!(
+                "Unable to find '{}' in package.json at: {:?}",
+                &search_word, source_file
+            );
+            return Err(error);
+        }
+    };
+    println!("Version read from package.json: '{}'", &version);
+
+    let fixed_version = match replace_pom_version(
+        &search_word,
+        &version,
+        source_file,
+        version_instance_count.clone(),
+    ) {
+        Ok(changed) => changed,
+        Err(error) => {
+            println!("Unable to update pom.xml! FUCK!");
+            return Err(error);
+        }
+    };
+    let mut target_file_write = match File::create(&target) {
+        Ok(file) => file,
+        Err(_) => {
+            println!("Unable to write to file at path: {:?}", target);
+            return Err(CustomError::IoError);
+        }
+    };
+    match target_file_write.write_all(fixed_version.as_bytes()) {
+        Ok(_) => {
+            println!(
+                "Version {} placed on the {} instance of {} in the pom.xml..",
+                &version, &version_instance_count, match_word
+            );
+            return Ok(());
+        }
+        Err(_) => {
+            println!("Unable to write to file at path: {:?}", target);
+            return Err(CustomError::IoError);
+        }
+    }
 }
 
-fn working_dir_params(args: Vec<String>) -> Result<PathBuf, CustomError> {
+fn handle_params(args: Vec<String>) -> Result<(PathBuf, PathBuf, u8), CustomError> {
     match args.len() {
-        3 if args[1] == "-p" => return Ok(PathBuf::from(args[2].clone())),
-        2 if args[1] == "-h" || args[1] == "-help" => {
-            println!("HELP HAS ARRIVED!");
-            println!("Call the binary on its own to use the calling directory as the search path.");
-            println!("Call the binary with the -p flag to specify a directory to search.");
-            println!("Example: <binary_name>");
-            println!("Example: <binary_name> -p /home/my_user/java_project/");
-            return Err(CustomError::BadParams);
+        //6 if args[1] == "-s" && args[3] == "-t" && args[4] == "-p" => {
+        4 => {
+            return Ok((
+                PathBuf::from(args[1].clone()),
+                PathBuf::from(args[2].clone()),
+                args[3].parse::<u8>().unwrap_or(0),
+            ))
         }
-        1 => return Ok(env::current_dir()?),
         _ => {
-            println!("I don't even know what you are trying to call 😂.");
-            println!("Call the binary on its own to use the calling directory as the search path.");
-            println!("Call the binary with the -p flag to specify a directory to search.");
-            println!("Example: <binary_name>");
-            println!("Example: <binary_name> -p /home/my_user/java_project/");
+            print_help_text();
             return Err(CustomError::BadParams);
         }
     };
 }
 
-fn version_from_package_json(search_word: &str, file: File) -> Result<String, CustomError> {
+fn print_help_text() {
+    println!("-----------------------------HELP-HAS-ARRIVED------------------------------------");
+    println!();
+    println!("This binary is designed to take the version number from your package.json");
+    println!("and replace the version in your pom.xml file. Since pom.xml has multiple version");
+    println!("tags, provide the tag number you would like replaced. The app counts instances of");
+    println!("tags with the pattern '<version>' and replaces the one you passed.");
+    println!();
+    println!("Usage:");
+    println!(
+        "      {} [package.json path] [pom.xml path] [version tag number]",
+        APP_NAME
+    );
+    println!();
+    println!("Example:");
+    println!(
+        "      {} /path/to/package.json /path/to/pom.xml <number>",
+        APP_NAME
+    );
+    println!();
+
+    println!("---------------------------------------------------------------------------------");
+}
+
+fn version_from_package_json(search_word: &str, file: &File) -> Result<String, CustomError> {
     let reader = BufReader::new(file);
     for line in reader.lines() {
         let line = line?;
@@ -128,8 +139,9 @@ fn version_from_package_json(search_word: &str, file: File) -> Result<String, Cu
 
 fn replace_pom_version(
     search_word: &str,
-    replacement_word: &str,
+    replacement_string: &str,
     file: File,
+    search_word_count: u8,
 ) -> Result<String, CustomError> {
     let mut counter = 0;
     let reader = BufReader::new(file);
@@ -137,11 +149,11 @@ fn replace_pom_version(
     for line in reader.lines() {
         let mut line = line?;
         if line.contains(search_word) {
-            if counter == 1 {
+            if counter == search_word_count {
                 let start_index = line.find(search_word).unwrap() + search_word.len();
                 let end_index = line.find("</").unwrap();
                 println!("Pom.xml previous line |{}", &line);
-                line.replace_range(start_index..end_index, replacement_word);
+                line.replace_range(start_index..end_index, replacement_string);
                 println!("Pom.xml replaced line |{}", &line);
             }
             counter += 1;
